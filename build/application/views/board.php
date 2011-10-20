7a5959e3b3536ea9b013adf175a1b040b7a51b03
James Wagoner <james.wagoner@happyhustle.com>
2011-10-20 18:25:53 -0400

    Updates sticky look per jimmy's help, added assigned to rough draft list, added jsp and quick flip. Added meta data side to sticky

--- build/application/views/board.php
+++ build/application/views/board.php
@@ -9,6 +9,7 @@
 	<link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon">
 	<link rel="stylesheet" media="all" type="text/css" href="/css/reset.css">
 	<link rel="stylesheet" media="all" type="text/css" href="/css/jquery-ui-1.8.16.custom.css">
+	<link rel="stylesheet" media="all" type="text/css" href="/css/jquery.jscrollpane.css">
 	<link rel="stylesheet" media="all" type="text/css" href="/css/uniform.default.css">
 	<link rel="stylesheet" media="all" type="text/css" href="/css/default.css">
 </head>
@@ -19,15 +20,15 @@
 		<div class="info">
 			<ul class="clearfix">
 				<li><select>
-					<option selected="selected" value="1">james wagoner (me)</option>
-					<option value="">all</option>
+					<option selected="selected">scrum view</option>
+					<option>james wagoner (me)</option>
 					<option>jimmy thomas</option>
 					<option>bryan conzone</option>
 				</select></li>
 				<li><select>
-					<option selected="selected">DealerNinja.com - Homepage</option>
-					<option>DN.com - </option>
-					<option>DealerNinja.com</option>
+					<option selected="selected">Project Stickea.se</option>
+					<option>DealerNinja.com - Homepage</option>
+					<option>CarNinja.com</option>
 				</select></li>
 				<li><a href="#">james wagoner ..</a></li>
 			</ul>
@@ -41,19 +42,92 @@
 				<h2>not started</h2>
 				<div>
 					<ul class="sticky_list">
-						<li><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
-						<li class="orange"><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
-						<li class="blue"><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
-						<li class="green"><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
-						<li class="grey"><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
-						<li class="pink"><p class="note">this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe  werkjhwer kwe</p><p class="separator"></p><p class="assigned"><a href="#">james wagoner</a></p></li>
+						<li class="sticky med">
+							<div class="qf">
+								<div class="front">
+									<div>
+										<p>this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe werkjhwer kwe</p>
+										<button class="flip">&nbsp;</button>
+									</div>
+								</div>
+								<div class="back">
+									<div>
+										<p><strong>Unassigned</strong></p>
+										<ul class="priority">
+											<li class="high">&nbsp;</li>
+											<li class="med">&nbsp;</li>
+											<li class="low">&nbsp;</li>
+										</ul>
+										<ul class="info">
+											<li class="created">Created: <span class="date">10/17/11 @ 12:14pm</span></li>
+										</ul>
+										<button class="delete">Delete</button>
+										<button class="done">Done</button>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="sticky low">
+							<div class="qf">
+								<div class="front">
+									<div>
+										<p>this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe werkjhwer kwe</p>
+										<button class="flip">&nbsp;</button>
+									</div>
+								</div>
+								<div class="back">
+									<div>
+										<p><strong>Unassigned</strong></p>
+										<ul class="priority">
+											<li class="high">&nbsp;</li>
+											<li class="med">&nbsp;</li>
+											<li class="low">&nbsp;</li>
+										</ul>
+										<ul class="info">
+											<li class="created">Created: <span class="date">10/17/11 @ 12:14pm</span></li>
+										</ul>
+										<button class="delete">Delete</button>
+										<button class="done">Done</button>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li class="sticky high">
+							<div class="qf">
+								<div class="front">
+									<div>
+										<p>this is a pending sticky! rawr rawr rawr rrrwqrwe wer ewr wer wer wer wer seruwer wer wek werkjhwerkwe werkjhwer kwe</p>
+										<button class="flip">&nbsp;</button>
+									</div>
+								</div>
+								<div class="back">
+									<div>
+										<p><strong>Unassigned</strong></p>
+										<ul class="priority">
+											<li class="high">&nbsp;</li>
+											<li class="med">&nbsp;</li>
+											<li class="low">&nbsp;</li>
+										</ul>
+										<ul class="info">
+											<li class="created">Created: <span class="date">10/17/11 @ 12:14pm</span></li>
+										</ul>
+										<button class="delete">Delete</button>
+										<button class="done">Done</button>
+									</div>
+								</div>
+							</div>
+						</li>
 					</ul>
 				</div>
 			</li>
-			<li id="inprogress">
-				<h2>in progress</h2>
+			<li id="assigned to">
+				<h2>assigned to</h2>
 				<div>
-					<ul class="sticky_list">
+					<ul class="user_list">
+						<li class="user">james wagoner</li>
+						<li class="user">jimmy thomas</li>
+						<li class="user">john doe</li>
+						<li class="user">jane doe</li>
 					</ul>
 				</div>
 			</li>
@@ -75,6 +149,9 @@
 <script type="text/javascript" src="/js/jquery-1.6.4.min.js"></script>
 <script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min.js"></script>
 <script type="text/javascript" src="/js/jquery.uniform.min.js"></script>
+<script type="text/javascript" src="/js/jquery.quickflip.min.js"></script>
+<script type="text/javascript" src="/js/jquery.jscrollpane.min.js"></script>
+<script type="text/javascript" src="/js/jquery.mousewheel.js"></script>
 <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
--- build/js/main.js
+++ build/js/main.js
@@ -8,7 +8,8 @@ $(function() {
 	});
 	
 	if ($('#board').length) {
-		var $content = $('#content'),
+		var borderWidth = 4,
+			$content = $('#content'),
 			footerHeight = $('#footer_wrapper'),
 			$winHeightUpd = $('#sticky_board, #sticky_board > li'),
 			$divHeightUpd = $('#sticky_board > li > div'),
@@ -17,16 +18,35 @@ $(function() {
 				var headerHeight = $('#header_wrapper').outerHeight(),
 					winHeight = (($(window).height() - headerHeight) - footerHeight),
 					divHeight = ((winHeight - $('#sticky_board h2:first').outerHeight(true)) - 16),
-					liWidth = Math.floor(($(window).width() - 2) / 3);
+					liWidth = Math.floor(($(window).width() - borderWidth) / 3);
 				
 				$content.height(winHeight);
 				$content.css('top', headerHeight + 'px');
 				$winHeightUpd.height(winHeight);
 				$divHeightUpd.height(divHeight).children('ul').css('min-height', divHeight + 'px');
-				$liWidthUpd.width(liWidth).last().width(($(window).width() - 2) - (liWidth * 2));
+				$liWidthUpd.width(liWidth).last().width(($(window).width() - borderWidth) - (liWidth * 2));
 			};
 		
 		$(window).resize(updateBoard);
 		updateBoard();
+		
+		$('#board').delegate('.sticky ul.priority li', 'click', function() {
+			var $this = $(this),
+				$sticky = $this.closest('.sticky');
+			
+			if (!$sticky.hasClass($this.attr('class'))) {
+				$sticky.removeClass('high med low').addClass($this.attr('class'));
+			}
+		});
+		
+		$('#board').delegate('.sticky div.front button.flip', 'click', function() {
+			$(this).closest('.qf').quickFlipper();
+		}).delegate('.sticky div.back button.done', 'click', function() {
+			$(this).closest('.qf').quickFlipper();
+		}).delegate('.sticky', 'mouseenter', function() {
+			$(this).addClass('hover');
+		}).delegate('.sticky', 'mouseleave', function() {
+			$(this).removeClass('hover');
+		});
 	}
 });
